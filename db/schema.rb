b176b6703069d98481230d4daeed9f35619198e2
torii-keisuke <keisuke.2001.12@gmail.com>
2022-05-26 20:22:58 +0900

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_24_065042) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_110623) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
