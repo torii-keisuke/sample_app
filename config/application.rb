fe311eaf66f40ebbf8620f0b1266fa80861b962b
torii-keisuke <keisuke.2001.12@gmail.com>
2022-06-01 15:28:34 +0900

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module SampleApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
